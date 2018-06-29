857818cf3bd4d45d59b1855137128cde4c7a315d
yuer5255 <295547598@qq.com>
2018-06-29 18:11:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import navbar from '@/components/navbar'
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style>
+@import "assets/style/icon.css";
+@import "assets/style/reset.css";
+#app {
+
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <mt-header>
+        <router-link to="/leftSide" slot="left">
+          <!--<mt-button icon="back">返回</mt-button>-->
+          <span class="toolbar-title-icon">
+            <i class="iconfont icon-list"></i>
+          </span>
+          <span>卖座电影</span>
+        </router-link>
+        <router-link to="/user" class="user iconfont icon-user" slot="right"></router-link>
+      </mt-header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navbar"
+    }
+</script>
+
+<style scoped lang="less">
+  .mint-header{
+    background-color: #282828;
+    height: 50px;
+    line-height: 50px;
+    color: #efefef;
+
+    .toolbar-title-icon{
+      box-shadow: 1px 0 1px #363636;
+      color: #999;
+      width: 48px;
+      margin-left: 10px;
+      margin-right: 30px;
+    }
+
+    .user{
+      color: #999;
+    }
+  }
+</style>
